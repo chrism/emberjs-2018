576a3967080e08a769646c1af683d2dc46e862f1
Chris Masters <chrismasters@gmail.com>
2018-06-05 19:57:15 +0200

    Glimmer not working

--- config/environment.js
+++ config/environment.js
@@ -15,7 +15,8 @@ module.exports = function(environment) {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
-        'ember-module-unification': true
+        'ember-module-unification': true,
+        "glimmer-custom-component-manager": true
       },
       EXTEND_PROTOTYPES: {
         // Prevent Ember Data from overriding Date.parse.
--- src/ui/components/tomster-logo/component.js
+++ src/ui/components/tomster-logo/component.js
@@ -1,9 +1,9 @@
-import Component from '@ember/component';
+import { CompatComponent as Component } from 'ember-glimmer-component';
 import { classNames } from '@ember-decorators/component';
 import { computed } from '@ember-decorators/object';
 
 @classNames('static-class')
-export default class TomsterLogo extends Component {
+export default class extends Component {
   @computed('filename', 'filetype')
   get tomsterPath() {
     return `/img/${this.filename}.${this.filetype}`
