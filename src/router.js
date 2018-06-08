0684201faf03f57ef035f2c3e2642ff2a8be318f
Chris Masters <chrismasters@gmail.com>
2018-06-08 15:33:40 +0200

    example with application-loading route

--- src/router.js
+++ src/router.js
@@ -6,6 +6,8 @@ const Router = EmberRouter.extend({
   rootURL: config.rootURL
 });
 
-Router.map(function() {});
+Router.map(function() {
+  this.route('application-loading');
+});
 
 export default Router;
