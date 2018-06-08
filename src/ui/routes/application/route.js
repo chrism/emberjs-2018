fcb1fd0910731ac9265a64231c38dcc0f4fb351b
Chris Masters <chrismasters@gmail.com>
2018-06-08 15:46:11 +0200

    Use imports

--- src/ui/routes/application/route.js
+++ src/ui/routes/application/route.js
@@ -1,8 +1,10 @@
 import Route from '@ember/routing/route';
+import { Promise } from 'rsvp';
+import { later } from '@ember/runloop';
 
 export default class ApplicationRoute extends Route {
   model() {
-    const delayPromise = new Ember.RSVP.Promise(resolve => Ember.run.later(resolve, 2000));
+    const delayPromise = new Promise(resolve => later(resolve, 2000));
 
     return delayPromise.then(() => {
       return { filename: "tomster", filetype: "png" };
