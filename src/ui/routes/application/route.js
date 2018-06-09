46c2bf378342bfffd864cbf7eb98e8d72c9298e1
Chris Masters <chrismasters@gmail.com>
2018-06-09 17:19:29 +0200

    Using delay on model

--- src/ui/routes/application/route.js
+++ src/ui/routes/application/route.js
@@ -1,7 +1,13 @@
 import Route from '@ember/routing/route';
+import { Promise } from 'rsvp';
+import { later } from '@ember/runloop';
 
 export default class ApplicationRoute extends Route {
   model() {
-    return { filename: "tomster", filetype: "png" }
+    const delayPromise = new Promise(resolve => later(resolve, 2000));
+
+    return delayPromise.then(() => {
+      return { filename: "tomster", filetype: "png" };
+    });
   }
 }
