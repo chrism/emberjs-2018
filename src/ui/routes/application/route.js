faea7130bc89d7cc60891b6c14b409b6b0d721fe
Chris Masters <chrismasters@gmail.com>
2018-06-10 20:00:01 +0200

    Remove get from route to access store

--- src/ui/routes/application/route.js
+++ src/ui/routes/application/route.js
@@ -1,9 +1,8 @@
 import Route from '@ember/routing/route';
-// import fetch from 'fetch';
 
 export default class ApplicationRoute extends Route {
   async model() {
-    let tomster = await this.get('store').find('tomster' ,1);
+    let tomster = await this.store.find('tomster' ,1);
 
     return tomster;
   }
